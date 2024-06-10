d18ce057b8fed0d925b336c0a88b3b064cbaa803
SebastianGallego <sebasglha@gmail.com>
2024-06-09 23:36:36 -0500

    adición de clases de prueba

---
+++ test/test_ControladorGasto.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestControladorGasto(TestCase):
+    def test_registrar_gasto(self):
+        self.fail()
+
+    def test_gasto_correcto(self):
+        pass
+
+    def test_gasto_presu_string(self):
+        pass
+
+    def test_gasto_presu_negativo(self):
+        pass
+
+    def test_gasto_fuera_de_tiempo(self):
+        pass
---
+++ test/test_ControladorViaje.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestControladorViaje(TestCase):
+    def test_registrar_viaje(self):
+        self.fail()
+
+    def test_registrar_viaje_correcto(self):
+        pass
+
+    def test_registrar_viaje_fechai_mayor_fechaf(self):
+        pass
